e37a151ce0316a9d0a4b1d328d8549a326fd3783
biswas-sid <80787459+biswas-sid@users.noreply.github.com>
2024-02-04 15:07:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
 
-os.environ['GOOGLE_API_KEY'] =  'AIzaSyAANEPA1UF6WE4O_0GQh2s27iBT4VrN0Ag'
+os.environ['GOOGLE_API_KEY'] =  'AIzaSyBzaCGAgYRMS5jpHoTJmyWFqq8TQJmOAFE'
 
 def get_pdf_text(pdf_docs):
     text=""
